ed01dfbf18e4370277487422f734f956780dc6cb
Linda Loftsgarden <linda-fintlabs@users.noreply.github.com>
2022-11-16 16:04:12 +0100

    FIK-10 added app bar

---
+++ src/features/main/Main.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+
+function Main() {
+    return (
+        <Box sx={{flexGrow: 1}}>
+            <AppBar>
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                        Kontroll
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
+
+export default Main;
+
+
+
