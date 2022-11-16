a570997a639fe1fe800c1d6d6937fe4c1122651b
Linda Loftsgarden <linda-fintlabs@users.noreply.github.com>
2022-11-16 22:30:17 +0100

    FIK-12 added colors to theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
 import Main from "./features/main/Main";
+import {createTheme, ThemeProvider} from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        secondary: {
+            light: '#7fb434',
+            main: '#5FA202',
+            dark: '#427101',
+        },
+        primary: {
+            light: '#4b727a',
+            main: '#1F4F59',
+            dark: '#15373e',
+        },
+    },
+    spacing: 8,
+    typography: {
+        fontFamily: [
+            "Nunito Sans", 'sans-serif'
+        ].join(',')
+    }
+});
 
 function App() {
     return (
-        <div>
+        <ThemeProvider theme={theme}>
             <Main/>
-        </div>
+        </ThemeProvider>
     );
 }
 
