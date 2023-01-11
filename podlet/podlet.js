823b4072cb73e6a4553401b22519ae1becb11450
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-01-11 22:14:51 +0100

    FIK-59 Improved logging and updated deps

--- podlet/podlet.js
+++ podlet/podlet.js
@@ -1,7 +1,10 @@
 const {podlet} = require("@fintlabs/fint-podium-react-podlet");
 const packageJson = require("./package.json");
-
+const log4js = require("log4js");
+const log = log4js.getLogger();
 const PODLET_NAME = process.env.PODLET_NAME || packageJson.name;
+
+log.level = process.env.LOGGING_LEVEL || "info"
 
 
 podlet.runPod(PODLET_NAME);
